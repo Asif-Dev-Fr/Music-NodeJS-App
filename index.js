617fc66e3d32e20784bd54f6ea6487631c4c7fff
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-04 11:47:45 +0200

    commit sur 'master'

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express();
 const fetch = require('node-fetch');
+require('dotenv').config();
 
 app.listen(3000, () => console.log('Listening at port 3000'));
 app.use(express.static('public'));
+
+const API_KEY = process.env.API_KEY
+
+app.get('/api', async (request, response) => {
+   const fetched_response = await fetch(API_KEY);
+   const fetched_data = await fetched_response.json();
+   response.json(fetched_data); 
+});
--- public/main.js
+++ public/main.js
@@ -1,1 +1,9 @@
 console.log('Client side');
+
+const data = async () => {
+    const response = await fetch('/api');
+    const data = await response.json();
+    console.log(data);
+}
+
+data();
