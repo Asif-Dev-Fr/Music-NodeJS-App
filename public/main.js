7e87c5c74bb2587b427fbd5fc2ccd7b817c17da0
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-07 12:48:38 +0200

    commit sur 'master'

--- public/main.js
+++ public/main.js
@@ -14,22 +14,32 @@ const search = async () => {
 
         const result = document.createElement('div');
         result.style.width = '33.3333%';
+        result.style.margin = '5px 0'
 
         document.getElementById('section-2').append(result);
 
         const artist = document.createElement('h2');
         artist.textContent = song.artist.name;
+        artist.style.textAlign = 'center';
         
         const cover = document.createElement('img');
-        cover.src = song.album.cover; 
+        cover.src = song.album.cover_medium; 
+        cover.style.display = 'block';
+        cover.style.margin = '0 auto';
+ 
 
         const title = document.createElement('h3');
         title.textContent = song.title;
+        title.style.textAlign = 'center';
+        title.style.margin = '5px 0';
 
         const previewSong = document.createElement('audio');
         previewSong.controls = 'controls';
         previewSong.src = song.preview;
-        previewSong.style.width = '90%';
+        previewSong.style.width = '90%'
+        previewSong.style.display = 'block';
+        previewSong.style.margin = '0 auto';
+
 
         result.append(artist, cover, title, previewSong);
 
