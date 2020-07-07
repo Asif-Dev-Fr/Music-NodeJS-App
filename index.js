bc153779c5a3e2d250e34d6c93b4092ea59a14ed
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-07 12:22:07 +0200

    commit sur 'master'

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ require('dotenv').config();
 app.listen(3000, () => console.log('Listening at port 3000'));
 app.use(express.static('public'));
 
-const API_KEY = process.env.API_KEY
 
-app.get('/api', async (request, response) => {
-   const fetched_response = await fetch(API_KEY);
+const API_KEY = process.env.API_KEY;
+
+app.get('/api/:searched', async (request, response) => {
+
+   const search = request.params.searched;
+
+   const fetched_response = await fetch(`${API_KEY}${search}`);
    const fetched_data = await fetched_response.json();
    response.json(fetched_data); 
 });
--- public/main.js
+++ public/main.js
@@ -1,9 +1,44 @@
-console.log('Client side');
+const search = async () => {
 
-const data = async () => {
-    const response = await fetch('/api');
+    let searched = document.getElementById('song').value;
+    console.log(searched);
+
+    const response = await fetch(`/api/${searched}`);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+
+    const songs = data.data;
+    console.log(songs);
+
+    songs.map((song) => {
+
+        const result = document.createElement('div');
+        result.style.width = '33.3333%';
+
+        document.getElementById('section-2').append(result);
+
+        const artist = document.createElement('h2');
+        artist.textContent = song.artist.name;
+        
+        const cover = document.createElement('img');
+        cover.src = song.album.cover; 
+
+        const title = document.createElement('h3');
+        title.textContent = song.title;
+
+        const previewSong = document.createElement('audio');
+        previewSong.controls = 'controls';
+        previewSong.src = song.preview;
+        previewSong.style.width = '90%';
+
+        result.append(artist, cover, title, previewSong);
+
+
+
+
+    });
+
 }
 
-data();
+const searchEvent = document.getElementById('searchButton');
+searchEvent.addEventListener('click', search)
