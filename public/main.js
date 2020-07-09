1312ce348b538c5eb783795b9bf00f3f2e17b338
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-09 13:34:01 +0200

    commit sur 'master'

--- public/main.js
+++ public/main.js
@@ -9,50 +9,28 @@ const search = async (e) => {
 
     const songs = data.data;
     console.log(songs);
-
-    songs.map((song) => {
-
-        const result = document.createElement('div');
-        result.style.width = '33.3333%';
-        result.style.margin = '10px 0'
-
-        document.getElementById('section-2').append(result);
+    
+    let result = '';
+    songs.map(song => {
         
-        const artist = document.createElement('h2');
-        artist.textContent = song.artist.name;
-        artist.style.textAlign = 'center';
-        artist.style.background = '#445565';
-        artist.style.color = '#FFF';
-        artist.style.width = '230px';
-        artist.style.margin = '0 auto';
-        artist.style.padding = '10px';
         
-        const cover = document.createElement('img');
-        cover.src = song.album.cover_medium; 
-        cover.style.display = 'block';
-        cover.style.margin = '0 auto';
- 
+        const artist = `<h2 class=artist-name >${song.artist.name}</h2>`;
+        const cover = `<img class="album-cover" src="${song.album.cover_medium}">`;
+        const title = `<h3 class="song-title">${song.title}</h3>`;
+        const previewSong = `<audio class="song-preview" controls src="${song.preview}"></audio>`
+  
 
-        const title = document.createElement('h3');
-        title.textContent = song.title;
-        title.style.textAlign = 'center';
-        title.style.padding = '10px 0';
-        
+        result += `<div class='song'>
+                            ${artist}
+                            ${cover}
+                            ${title}
+                            ${previewSong}
+                        </div>`;
+        document.getElementById("section-2").innerHTML = result;
+    })
 
-        const previewSong = document.createElement('audio');
-        previewSong.controls = 'controls';
-        previewSong.src = song.preview;
-        previewSong.style.width = '90%'
-        previewSong.style.display = 'block';
-        previewSong.style.margin = '0 auto';
-
-
-        result.append(artist, cover, title, previewSong);
-
-        document.querySelector('form').reset();
-
-    });
-
+    document.querySelector('form').reset();
+    
 }
 
 const searchEvent = document.querySelector('form');
