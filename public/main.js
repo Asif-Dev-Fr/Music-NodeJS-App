460bc1ce93889201377ee0543d3ce45ca90dad60
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-10 11:26:58 +0200

    commit sur 'master'

--- public/main.js
+++ public/main.js
@@ -13,19 +13,18 @@ const search = async (e) => {
     let result = '';
     songs.map(song => {
         
-        
         const artist = `<h2 class=artist-name >${song.artist.name}</h2>`;
-        const cover = `<img class="album-cover" src="${song.album.cover_medium}">`;
+        const cover = `<img class="album-cover" src="${song.album.cover_medium} alt="${song.title}">`;
         const title = `<h3 class="song-title">${song.title}</h3>`;
         const previewSong = `<audio class="song-preview" controls src="${song.preview}"></audio>`
   
 
         result += `<div class='song'>
-                            ${artist}
-                            ${cover}
-                            ${title}
-                            ${previewSong}
-                        </div>`;
+                        ${artist}
+                        ${cover}
+                        ${title}
+                        ${previewSong}
+                    </div>`;
         document.getElementById("section-2").innerHTML = result;
     })
 
